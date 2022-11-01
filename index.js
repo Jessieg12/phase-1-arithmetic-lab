d221cb15d166482d7399cf721b61e7133804d37c
Jessie Guzman <jessieguzman777@gmail.com>
2022-11-01 18:28:41 -0500

    commit tree 6828fdba6c084d4301da1465eed7f1e503dd2706

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
+let num1= 31
+let num2= 2
+let multiply= num1*num2
 
-const num1 = 31;
-const num2 = 2;
-const multiply = num1 * num2
+let random = Math.floor(Math.random()*10)+1
+
+let num3 = 20
+let num4 = 5
+let mod = num3/num4
 
 
-const random = Math.ceil(Math.random());
-
-const num3 = 4;
-const num4 = 1;
-const mod = num3 / num4
-
-const max = Math.max(1, 2, 3, 4, 5, 6, 20);
+let max = Math.max(1,2,3,4,5,6,7,8,9,20)
